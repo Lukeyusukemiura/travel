26f3ccf0920f94eb52da6aa010884a6f6aee0869
Yusuke Miura <yusukemiura34c@gmail.com>
2022-03-23 18:57:28 +0900

    photo added

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,87 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>hello world</p>
+  <div class="box bg-gray-800">
+    <div class="upper-box snap-x">
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+      <div
+        class="bg-white m-3 card overflow-hidden border snap-center rounded-lg"
+      >
+        <div class="flex justify-center items-center">
+          <img class="photo" src="../images/photo1.jpg" />
+        </div>
+        <div class="p-5">
+          <h4>Wakayama prefecture</h4>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.box {
+  width: 100%;
+  height: 100vh;
+}
+.card {
+  height: 400px;
+  width: 300px;
+}
+.photo {
+  height: auto;
+  width: 300px;
+}
+.upper-box {
+  width: 100%;
+  height: 450px;
+}
+</style>
